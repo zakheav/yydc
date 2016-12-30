3ce7e43cb5d9d8570cf84feb83f77370a2c43854
weiyuan <18810543471@163.com>
2016-12-30 15:38:17 +0800

    new

--- src/preprocessHandler/ProbeDataPreprocessHandler.java
+++ src/preprocessHandler/ProbeDataPreprocessHandler.java
@@ -1,10 +1,8 @@
 package preprocessHandler;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import dbpool.Query;
+import java.util.Set;
 import preprocess.Handler;
 import serverUtil.MemCache;
 import serverUtil.Sql;
@@ -20,49 +18,33 @@ public class ProbeDataPreprocessHandler implements Handler {
 	@Override
 	public void preprocess(Object params) {
 		Sql sql = (Sql) params;
-		String boxMac = (sql.get_paramsList().get(0).get(0).split("_"))[1];// 上传信息的盒子
+
 		Long now = get_now();// 当前时间段的起始
 
-		// 搜索当前上传信息的盒子在这一小时的周围人数
-		List<Map<String, Object>> results = new Query().select("*").table("yydc_probe1").where("probe_time").bigger(now)
-				.andWhere("box_mac").equal(boxMac).fetchAll();
-		Integer userCount = 0;
-		Map<String, Boolean> hash = new HashMap<String, Boolean>();// 用于去重
-		for (int i = 0; i < results.size(); ++i) {
-			String mac = (String) results.get(i).get("mac");
-			if (hash.get(mac) == null) {
-				hash.put(mac, true);
-				++userCount;
-			}
-		}
-		results = new Query().select("*").table("yydc_probecache").where("box_mac").equal(boxMac).andWhere("time")
-				.equal(now).fetchAll();
-		if (results.isEmpty()) {
-			List<Object> p = new ArrayList<Object>();
-			p.add(now);
-			p.add(boxMac);
-			p.add(userCount);
-			new Query().insertInto("yydc_probecache(time, box_mac, count)").values(p).executeUpdate();
-		} else {
-			HashMap<String, Object> attr_value = new HashMap<String, Object>();
-			attr_value.put("count", userCount);
-			new Query().update("yydc_probecache").set(attr_value).where("box_mac").equal(boxMac).andWhere("time")
-					.equal(now).executeUpdate();
+		if (now > MemCache.getInstance().thisHour) {// 到了下一个小时，把box_customer的数据写到硬盘上
+			MemCache.getInstance().storeToDB(now);
 		}
 
-		// 在redis数据库中缓存盒子当前周围人数
-		int probeNum = 0;
-		long createTime = System.currentTimeMillis() / 1000;
-		hash = new HashMap<String, Boolean>();// 用于去重
-		List<List<String>> paramsList = sql.get_paramsList();
-		for (int i = 0; i < paramsList.size(); ++i) {
-			List<String> p = paramsList.get(i);
-			String mac = p.get(2).split("_")[1];
-			if (hash.get(mac) == null) {
-				++probeNum;
-				hash.put(mac, true);
+		if (!sql.get_paramsList().isEmpty()) {
+			long createTime = System.currentTimeMillis() / 1000;
+			Set<String> set = new HashSet<String>();// 用于去重
+			List<List<String>> paramsList = sql.get_paramsList();
+
+			String boxMac = (sql.get_paramsList().get(0).get(0).split("_"))[1];// 上传信息的盒子
+			if (!MemCache.getInstance().box_customer.containsKey(boxMac)) {
+				MemCache.getInstance().box_customer.put(boxMac, new HashSet<String>());
 			}
+			for (List<String> param : paramsList) {
+				String mac = (param.get(2).split("_"))[1];
+				Integer rssi = Integer.parseInt((param.get(3).split("_"))[1]);
+				if (rssi > -70) {
+					MemCache.getInstance().box_customer.get(boxMac).add(mac);
+					set.add(mac);
+				}
+			}
+			int probeNum = set.size();
+			
+			MemCache.getInstance().set_boxProbenumberNow(boxMac, createTime, probeNum);// 记录当前盒子的在线数目
 		}
-		MemCache.getInstance().set_boxProbenumberNow(boxMac, createTime, probeNum);// 记录当前盒子的在线数目
 	}
 }
--- src/serverUtil/MemCache.java
+++ src/serverUtil/MemCache.java
@@ -1,24 +1,49 @@
 package serverUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+
+import dbpool.Query;
 
 public class MemCache {
 	private static MemCache instance = new MemCache();
 
 	private final ConcurrentMap<String, Integer> box_probeNum;
 	private final ConcurrentMap<String, Long> box_timer;
+	public Map<String, Set<String>> box_customer;// 缓存这一小时每个盒子的访客
+	public Long thisHour;
 
 	private MemCache() {
 		box_probeNum = new ConcurrentHashMap<String, Integer>();
 		box_timer = new ConcurrentHashMap<String, Long>();
+		box_customer = new HashMap<String, Set<String>>();
+		thisHour = (System.currentTimeMillis() / (1000 * 3600)) * 3600;
 	}
 
 	public static MemCache getInstance() {
 		return instance;
 	}
 
-	public Long get_boxTimeStamp(String mac) {
+	public synchronized void storeToDB(Long now) {
+		if (now > thisHour) {
+			for (String boxMac : box_customer.keySet()) {
+				Integer userCount = box_customer.get(boxMac).size();
+				ArrayList<Object> params = new ArrayList<Object>();
+				params.add(MemCache.getInstance().thisHour);
+				params.add(boxMac);
+				params.add(userCount);
+				new Query().insertInto("yydc_probecache(time, box_mac, count)").values(params).executeUpdate();
+			}
+			box_customer = new ConcurrentHashMap<String, Set<String>>();
+			thisHour = now;
+		}
+	}
+
+	private Long get_boxTimeStamp(String mac) {
 		Long timestamp = box_timer.get(mac);
 		if (timestamp == null)
 			timestamp = new Long(0);
@@ -28,7 +53,7 @@ public class MemCache {
 	public Integer get_boxProbenumberNow(String mac) {
 		Long timestamp = get_boxTimeStamp(mac);
 		long nowTime = System.currentTimeMillis();
-		if (nowTime - timestamp < 600000) {
+		if (nowTime - timestamp < 600 * 1000) {
 			return box_probeNum.get(mac);
 		} else {
 			return 0;
